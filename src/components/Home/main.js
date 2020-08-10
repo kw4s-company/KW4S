6f5ac7e0bae5f12562437cace57183ed1db8f53e
kw4s-company <kw4s.com@gmail.com>
2020-08-09 19:43:35 -0700

    added some borders

--- src/components/Home/main.js
+++ src/components/Home/main.js
@@ -102,6 +102,7 @@ export default class main extends Component {
               
             </div>
             <div className="main-text">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#06c28a" fill-opacity="1" d="M0,288L48,282.7C96,277,192,267,288,240C384,213,480,171,576,149.3C672,128,768,128,864,154.7C960,181,1056,235,1152,240C1248,245,1344,203,1392,181.3L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
               <div className="main-text1">
               <h1 className=""> شركة خطاب</h1>
               <p>
