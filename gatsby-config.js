2b08bc7a3b16acdbe5cb5300f350f19985dc685a
kw4s-company <kw4s.com@gmail.com>
2020-08-11 06:00:43 -0700

    trying without offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,17 +48,8 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/logok4ws.png`, // This path is relative to the root of the site.
-        cache_busting_mode: 'none'
       },
     },
-    {
-      resolve: 'gatsby-plugin-offline',
-      options: {
-         workboxConfig: {
-            globPatterns: [`src/images/logok4ws.png`]
-         }
-      }
-   }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
